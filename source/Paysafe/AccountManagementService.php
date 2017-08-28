85af40a4a300a705e0ae875cb00f8f88ae119219
jbPayLease <31292494+jbPayLease@users.noreply.github.com>
2017-08-28 11:25:22 -0700

    split-pay Add AccountManagementService to SDK
    Adding a new service to the SDK for Account Management. This commit
    includes the functions ping, transferCredit, and transferDebit. Transfer
    functions can be used to move money between PaySafe master and
    sub-accounts. The ping function can be used to determine if the
    Account Management service is available.

---
+++ source/Paysafe/AccountManagementService.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Paysafe;
+
+use Paysafe\AccountManagement\Transfer;
+
+class AccountManagementService
+{
+    /**
+     * @var PaysafeApiClient
+     */
+    private $client;
+
+    /**
+     * The uri for the card payment api.
+     * @var string
+     */
+    private $uri = "accountmanagement/v1";
+    private $debitPath = "/debits";
+    private $creditPath = "/credits";
+
+    /**
+     * Initialize the Account Management service.
+     *
+     * @param \Paysafe\PaysafeApiClient $client
+     */
+    public function __construct( PaysafeApiClient $client )
+    {
+        $this->client = $client;
+    }
+
+    public function transferDebit(Transfer $transfer)
+    {
+        $request = new Request(array(
+               'method' => Request::POST,
+               'uri' => $this->prepareURI($this->debitPath),
+               'body' => $transfer
+           ));
+
+        $response = $this->client->processRequest($request);
+
+        return new Transfer($response);
+    }
+
+    public function transferCredit(Transfer $transfer)
+    {
+        $request = new Request(array(
+               'method' => Request::POST,
+               'uri' => $this->prepareURI($this->creditPath),
+               'body' => $transfer
+           ));
+
+        $response = $this->client->processRequest($request);
+
+        return new Transfer($response);
+    }
+
+    private function prepareURI($path)
+    {
+        if (!$this->client->getAccount())
+        {
+            throw new PaysafeException('Missing or invalid account', 500);
+        }
+
+        return $this->uri . "/accounts/" . $this->client->getAccount() . $path;
+    }
+}
