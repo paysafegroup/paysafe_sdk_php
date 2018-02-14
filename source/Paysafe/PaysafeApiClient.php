2de4b99129f4ac454aeb2796aea53b8c3092fd63
Richard Parnaby-King <cintaria@gmail.com>
2018-02-14 11:41:13 +0000

    Updated api endpoints
    The api.paysafe.com urls are no longer valid and perform a 302 redirect to the paysafe homepage. The endpoints in the paysafe api documentation have been updated to api.netbanx.com. The endpoints in the code need updating.

--- source/Paysafe/PaysafeApiClient.php
+++ source/Paysafe/PaysafeApiClient.php
@@ -117,9 +117,9 @@ class PaysafeApiClient
         $this->environment = $environment;
 
         if ($this->environment == Environment::TEST) {
-            $this->apiEndPoint = "https://api.test.paysafe.com";
+            $this->apiEndPoint = "https://api.test.netbanx.com";
         } else {
-            $this->apiEndPoint = "https://api.paysafe.com";
+            $this->apiEndPoint = "https://api.netbanx.com";
         }
 
         $this->account = $account;
