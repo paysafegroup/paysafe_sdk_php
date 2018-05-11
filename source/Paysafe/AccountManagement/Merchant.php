1166c5e87001a65b8cedc7d0549c9b4552c432fd
Kostya Polyakov <kostya.p.dev@gmail.com>
2018-05-11 17:14:45 +0300

    RT-5679 Create top-level merchant for each PM we onboard

---
+++ source/Paysafe/AccountManagement/Merchant.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Paysafe\AccountManagement;
+
+/**
+ * Class Merchant
+ * @package Paysafe\AccountManagement
+ *
+ * @property string $id
+ * @property string $name
+ * @property \Paysafe\Link[] $links
+ */
+class Merchant extends \Paysafe\JSONObject implements \Paysafe\Pageable {
+
+    public static function getPageableArrayKey() {
+        return "merchant";
+    }
+
+    protected static $fieldTypes = array(
+        'id' => 'string',
+        'name' => 'string',
+        'links' => 'array:\Paysafe\Link'
+    );
+}
